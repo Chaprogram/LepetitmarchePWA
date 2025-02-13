31bf9f22eb15dbc31b37e33f0476c5ac6be49aa1
Charline <charlinec03@gmail.com>
2025-02-13 23:12:29 +0100

    run

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 from PMapp import create_app, socketio,db
 from flask_migrate import Migrate
 import os
-
+from waitress import serve
 
 # Créer l'application Flask
 
@@ -14,7 +14,5 @@ migrate = Migrate(PMapp,db)
 print(PMapp.url_map)  # Liste toutes les routes connues
 
 if __name__ == '__main__':
-    PMapp.run(debug=False, use_reloader=False)
-    from waitress import serve
     port = int(os.environ.get("PORT", 5000)) 
-    serve(PMapp, host="0.0.0.0", port=5000)
+    serve(PMapp, host="0.0.0.0", port=port)
