a2ff924d13634f1b924c784886a39c9a73e80427
Charline <charlinec03@gmail.com>
2025-02-17 21:11:57 +0100

    init

--- backend/PMapp/__init__.py
+++ backend/PMapp/__init__.py
@@ -4,6 +4,8 @@ from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_mail import Mail
 import os
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 # Initialisation des extensions
 db = SQLAlchemy()
@@ -15,6 +17,9 @@ def create_app():
     # Créer l'application Flask
     app = Flask(__name__)
 
+# the toolbar is only enabled in debug mode:
+    app.debug = True
+
     # Configuration de l'application
     db_url = os.getenv('DATABASE_URL')
     if db_url and db_url.startswith("postgres://"):
@@ -23,6 +28,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback_clé_secrète')  
+
+    toolbar = DebugToolbarExtension(app)
 
     # Configuration de Flask-Mail
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'  
@@ -39,7 +46,10 @@ def create_app():
     socketio.init_app(app)
     mail.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'main.login'  
+    login_manager.login_view = 'main.login'
+    toolbar.init_app(app) 
+     
+
 
     # Création de la base de données (tables)
     with app.app_context():
