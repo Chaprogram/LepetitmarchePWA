e0d287175b5d6ab3a50982bfca6f5a35c2151157
Charline <charlinec03@gmail.com>
2025-02-19 14:15:43 +0100

    run

--- backend/run.py
+++ backend/run.py
@@ -1,20 +1,26 @@
-from PMapp import create_app,db
+from PMapp import create_app, db
 from flask_migrate import Migrate
 import os
 from waitress import serve
 import logging
-logging.basicConfig(level=logging.DEBUG)
+import sys
+
+# Configurer les logs pour qu'ils s'affichent dans stdout
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 # Créer l'application Flask
-
 PMapp = create_app()
 
-migrate = Migrate(PMapp,db)
+migrate = Migrate(PMapp, db)
 
+# Récupérer le port à partir de la variable d'environnement
+port = int(os.environ.get("PORT", 5000))
 
-# Afficher les routes pour le débogage
-print(PMapp.url_map)  # Liste toutes les routes connues
+# Afficher les routes pour le débogage (uniquement en mode debug)
+if PMapp.config["DEBUG"]:
+    print(PMapp.url_map)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) 
+    # Lancer l'application avec waitress
     serve(PMapp, host="0.0.0.0", port=port, threads=4)
+
