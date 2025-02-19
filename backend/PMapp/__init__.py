7f3fd7bd10b0cdf21e629bb21354411b1e22a899
Charline <charlinec03@gmail.com>
2025-02-19 20:28:34 +0100

    jespere

--- backend/PMapp/__init__.py
+++ backend/PMapp/__init__.py
@@ -5,8 +5,7 @@ from flask_socketio import SocketIO
 from flask_mail import Mail
 import os
 from dotenv import load_dotenv
-from .routes import main
-from . import db
+
 
 load_dotenv()
 # Initialisation des extensions
@@ -61,7 +60,7 @@ def create_app():
         return User.query.get(int(user_id))
 
     # Importation et enregistrement des routes
-   
+    from .routes import main
     app.register_blueprint(main)
 
     return app
