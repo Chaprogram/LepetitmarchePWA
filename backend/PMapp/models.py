dd7bf328ac9717b1f91ecba749d6779eef2fa483
Charline <charlinec03@gmail.com>
2025-02-19 19:53:58 +0100

    reinita

--- backend/PMapp/models.py
+++ backend/PMapp/models.py
@@ -5,17 +5,20 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 
+db = SQLAlchemy()
 
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)  # Champ pour distinguer les administrateurs
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', admin={self.is_admin})"
+        return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
