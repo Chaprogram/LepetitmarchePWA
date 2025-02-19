ecb2e5cb86010b2474e936e1af25ee7143ebb3bf
Charline <charlinec03@gmail.com>
2025-02-19 21:37:11 +0100

    model

--- backend/PMapp/models.py
+++ backend/PMapp/models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 
-db = SQLAlchemy()
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
