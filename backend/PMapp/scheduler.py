e42e580a5eceb9b7b6e40707d3321d8494810127
Charline <charlinec03@gmail.com>
2025-03-05 21:19:19 +0100

    no open port

--- backend/PMapp/scheduler.py
+++ backend/PMapp/scheduler.py
@@ -1,17 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 import pytz
-from datetime import datetime
 from .utils import send_admin_report  # Assurez-vous que la fonction pour envoyer l'email est correctement importée
+import threading
 
 def schedule_report():
-    scheduler = BackgroundScheduler(timezone=pytz.timezone('Europe/Paris'))  # Définir le fuseau horaire sur Paris
+    # Définir le fuseau horaire sur Paris
+    scheduler = BackgroundScheduler(timezone=pytz.timezone('Europe/Paris'))  
     
-    # Planifie le job à 15:00 heure locale de Paris
+    # Planifier le job pour l'exécution quotidienne à 15:00, heure locale de Paris
     scheduler.add_job(func=send_admin_report, trigger="cron", hour=15, minute=0)
-    scheduler.start()
-
-    # Ajoute un gestionnaire pour vérifier l'exécution des jobs
+    
+    # Ajouter un listener pour les événements d'exécution des jobs
     def job_listener(event):
         if event.exception:
             print(f"Erreur lors de l'exécution du job: {event.job_id}")
@@ -20,11 +20,9 @@ def schedule_report():
 
     scheduler.add_listener(job_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
 
+    # Lancer le scheduler dans un thread séparé pour qu'il n'empêche pas Flask de tourner
+    def run_scheduler():
+        scheduler.start()
 
-    # Veiller à ne pas arrêter le planificateur lors de la fermeture de l'application
-    try:
-        while True:
-            pass
-    except (KeyboardInterrupt, SystemExit):
-        scheduler.shutdown()
-
+    scheduler_thread = threading.Thread(target=run_scheduler)
+    scheduler_thread.start()
--- backend/run.py
+++ backend/run.py
@@ -1,24 +1,26 @@
-from PMapp import create_app,db
+from PMapp import create_app, db
 from flask_migrate import Migrate
 import os
 from waitress import serve
 import logging
-logging.basicConfig(level=logging.DEBUG)
 from PMapp.scheduler import schedule_report
 
 # Créer l'application Flask
-
 PMapp = create_app()
 
-migrate = Migrate(PMapp,db)
+# Initialisation de Migrate
+migrate = Migrate(PMapp, db)
 
+# Initialisation du scheduler
 schedule_report()
 
+# Récupérer le port à partir de l'environnement (Render fournit un port dynamique)
+port = int(os.environ.get("PORT", 5000))
 
-port = int(os.environ.get("PORT", 5000)) 
-# Afficher les routes pour le débogage
+# Afficher les routes pour débogage
 print(PMapp.url_map)  # Liste toutes les routes connues
 
+# Lancer l'application avec Waitress
 if __name__ == '__main__':
- 
     serve(PMapp, host="0.0.0.0", port=port, threads=4)
+
